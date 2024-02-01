f87883560724daa913adda51510329b3bce4fa0e
AlanHenriqueRF <alhenrangelfera@gmail.com>
2024-02-01 14:58:53 -0300

    style: Styles of NavBar, with the annimation on logo and link to especific pages

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,9 +1,11 @@
+import Reset from "../styles/reset.css";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
+            <Reset/>
             <NavBar />
             <>
                 {children}
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,117 @@
+import styled from 'styled-components';
+import favicon from '../../public/favicon.ico'
+import logo from '../../public/images/white-pokeball.svg'
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+interface LogoProps {
+    tamanho: '259px' | '62px';
+}
+
 export default function NavBar() {
+    const [aberto, setAberto] = useState(true)
+
+    useEffect(() => {
+        setTimeout(() => {
+            console.log('deu 5')
+            setAberto(false)
+        }, 5000)
+    }, [])
+    
     return (
         <>
-            <h1>NavBar</h1>
+            <Blocknav>
+                <PokemonLogo tamanho={aberto ? '259px' : '62px'} onMouseOver={() => { setAberto(true) }} onMouseOut={() => setAberto(false)}>
+                    <Link href='/'>
+                        <img src={logo.src} alt="" />
+                        {aberto ? <h1>Centro Pokémon </h1> : <></>}
+                    </Link>
+                </PokemonLogo>
+                <Servicos>
+                    <Link href='/quem-somos'>
+                        <h1>Quem Somos</h1>
+                    </Link>
+                    <Link href={'/agendar-consulta'}>
+                        <div>
+                            <h1>Agendar Consulta</h1>
+                        </div>
+                    </Link>
+                </Servicos>
+            </Blocknav>
         </>
     )
 }
+
+const Blocknav = styled.div`
+    height: 104px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-family: "Inter", sans-serif;
+    a{
+        text-decoration: none;
+        color: #000000;
+        cursor: pointer;
+    }
+`
+
+const PokemonLogo = styled.div<LogoProps>`
+    margin-left: 83px;
+    width: ${props => props.tamanho} ;
+    height: 62px;
+    background-color: #E40F0F;
+    border-radius: 50px;
+    display: flex;
+    align-items: center;
+    animation-name: formatoReduzido;
+    animation-duration: 1.5s;
+    animation-delay: 5s;
+    @keyframes formatoReduzido {
+        0%   {width: 259px;}
+        100% {width: 62px;}
+    }
+    a{
+        display: flex;
+        align-items: center;
+        img{
+            width: 37px;
+            height: 34px;
+            margin-left: 13px;
+            }
+        h1{
+            margin-left: 19px;
+            color: #FFFFFF;
+            font-size: 20px;
+            font-weight: 600;
+            }
+    }
+    
+`
+
+const Servicos = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 291px;
+    margin-right: 83px;
+    h1{
+        color: #000000;
+        font-size: 14px;
+        font-weight: 400;
+    }
+    a{
+        div{
+            width: 172px;
+            height: 42px;
+            border-radius: 30px;
+            background-color: #E40F0F;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            h1{
+                color: #FFFFFF;
+                font-weight: 700;
+            }
+        }
+    }
+`
