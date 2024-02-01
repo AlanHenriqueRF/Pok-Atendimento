77cadbaa9da14aaa113128d0c98765c3c71feaf6
AlanHenriqueRF <alhenrangelfera@gmail.com>
2024-02-01 16:38:50 -0300

    style: Styles of Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,28 @@
+import { styled } from "styled-components"
+
 export default function Footer() {
     return (
         <>
-            <h1>final da pagina</h1>
+            <Footerbox>
+                <h1>Todas as marcas e ilustrações utilizadas são de seus resepctivos donos.</h1>
+            </Footerbox>
         </>
     )
 }
+
+const Footerbox = styled.div`
+    display: flex;
+    position: fixed;
+    bottom:0;
+    width:100vw;
+    height:72px;
+    align-items: center;
+    justify-content:center;
+    background-color: #1D1D1D;
+    font-family: "Inter", sans-serif;
+    h1{
+        color: #FFFFFF;
+        font-size: 14px;
+        font-weight: 400;
+    }
+`
